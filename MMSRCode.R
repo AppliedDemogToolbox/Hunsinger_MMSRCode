c9e0b69ee5205341d34b2ec8a96b129f6a7dd142
Applied Demography Toolbox <45884231+AppliedDemogToolbox@users.noreply.github.com>
2020-06-30 23:40:43 -0700

    Update MMSRCode.R

--- MMSRCode.R
+++ MMSRCode.R
@@ -1,7 +1,7 @@
 ##############################################################################################################################
-##EDDIE'S R CODE FOR FITTING THE MULTI-EXPONENTIAL MODEL MIGRATION SCHEDULE WITH STUDENT PEAK
+##EDDIE'S R CODE FOR FITTING THE MULTI-EXPONENTIAL MODEL MIGRATION SCHEDULE
 ##
-##EDDIE HUNSINGER, AUGUST 2018 (LAST UPDATED OCTOBER 2019)
+##EDDIE HUNSINGER, AUGUST 2018 (LAST UPDATED JUNE 2020)
 ##http://www.demog.berkeley.edu/~eddieh/
 ##
 ##IF YOU WOULD LIKE TO USE, SHARE OR REPRODUCE THIS CODE, BE SURE TO CITE THE SOURCE
@@ -20,11 +20,15 @@
 ##IF YOU GET A POOR FIT FOR SOME DATA, ONE ITEM TO REVIEW IS COMPARISON OF THE PARAMETER ESTIMATES TO THE STARTING PARAMETER DISTRIBUTIONS (MAY NEED TO SIMPLY EXPAND OR CHANGE THE BOUNDS OF AN INPUT)
 ##
 ##COPIES WITH SOME APPLICATION OF AND COMPARISON TO R's lm() AND nls() FUNCTIONS, AND TO ~PLAIN MONTE CARLO, 
-##ARE AVAILABLE AT https://github.com/AppliedDemogToolbox/Hunsinger_SPMMSRCode/tree/master/FittingComparisons
+##ARE AVAILABLE AT https://github.com/AppliedDemogToolbox/Hunsinger_MMSRCode/tree/master/FittingComparisons
 ##
-##FOR MORE INFO ON THE SPMMS MODEL, SEE: Wilson, T. (2010). “Model migration schedules incorporating student migration peaks.” Demographic Research, 23(8): 191–222.
+##FOR MORE INFO ON THE SPMMS MODEL, SEE: 
+##-Wilson, T. (2010). “Model migration schedules incorporating student migration peaks.” Demographic Research, 23(8): 191–222.
 ##AVAILABLE ONLINE: https://www.demographic-research.org/Volumes/Vol23/8/default.htm
-##RELATED EXCEL WORKBOOK BY TOM WILSON: http://www.demog.berkeley.edu/~eddieh/toolbox.html#SPMMS (MANY THANKS TO TOM WILSON FOR SHARING IT - A TERRIFIC RESOURCE AND GUIDE)
+##-Wilson, T. (2020). “Modelling Age Patterns of Internal Migration at the Highest Ages.” Spatial Demography, 8.
+##AVAILABLE ONLINE: rdcu.be/b5baq
+##
+##RELATED EXCEL WORKBOOK BY TOM WILSON: https://figshare.com/articles/Model_Migration_Schedule_fitting_example/12415475
 ##(NOTE BELOW HAS PARAMETERS SET TO RUN ALL STEPS AS DEFAULT BUT THE EXCEL WORKBOOK HAS ELDERLY STEP TURNED OFF AS DEFAULT)
 ##
 ##GREAT RESOURCE ON THE ROGERS-CASTRO MODEL: Rogers A & Castro LJ (1981). Model Migration Schedules. IIASA Research Report. IIASA, Laxenburg, Austria: RR-81-030
@@ -44,8 +48,8 @@
 
 ###############
 #DATA FROM WILSON (2010)
-SPMMSTestingData<-read.table(file="https://github.com/AppliedDemogToolbox/Hunsinger_SPMMSRCode/raw/master/SPMMSData.csv",header=TRUE,sep=",")
-migprob<-(SPMMSTestingData$Migration.probability[1:90])
+SPMMSTestingData<-read.table(file="https://github.com/AppliedDemogToolbox/Hunsinger_MMSRCode/raw/master/MMSData.csv",header=TRUE,sep=",")
+migprob<-(SPMMSTestingData$Migration.intensity[1:105])
 
 #SIZE OF migprob (DATA BY AGE) USED
 SIZE<-90
@@ -128,16 +132,20 @@ retparam3tries<-array(runif(TRIES,55,65)) #retparam1tries<-array(runif(TRIES,55,
 ###############
 ##STEP 6 INPUTS
 #MIN AND MAX OF ELDERLY AGES TO FIT OVER
-eldmin<-70
-eldmax<-90
+eldmin<-75
+eldmax<-105
 
 #HEIGHT OF THE ELDERLY CURVE
 #TO EXCLUDE ELDERLY CURVE FROM MODEL CAN SET LOW AS '0' AND HIGH AS '1e-10'
-eldparam1tries<-array(runif(TRIES,0,.000005)) #eldparam1tries<-array(runif(TRIES,0,1e-10))
+eldparam1tries<-array(runif(TRIES,0,.02)) #eldparam1tries<-array(runif(TRIES,0,1e-10))
 
-#RATE OF ASCENT OF THE ELDERLY CURVE
-#TO EXCLUDE ELDERLY CURVE FROM MODEL CAN SET LOW AS '0' AND HIGH AS '1e-10'
-eldparam2tries<-array(runif(TRIES,.05,.1)) #eldparam2tries<-array(runif(TRIES,0,1e-10))
+#RATE OF DESCENT OF ELDERLY CURVE
+#TO APPROXIMATELY EXCLUDE ELDERLY CURVE FROM MODEL CAN SET LOW AS '0' AND HIGH AS '1e-10'
+eldparam2tries<-array(runif(TRIES,2.5,20)) #eldparam2tries<-array(runif(TRIES,0,1e-10))
+
+#POSITION OF THE ELDERLY CURVE ON THE AGE-AXIS
+#TO APPROXIMATELY EXCLUDE ELDERLY CURVE FROM MODEL CAN SET LOW AS '100' AND HIGH AS '+1e-10'
+eldparam3tries<-array(runif(TRIES,85,105)) #eldparam1tries<-array(runif(TRIES,100,100+1e-10))
 ###############
 
 ###############
@@ -284,33 +292,36 @@ step5best<-array(step1-step4,dim=c(length(step1)))
 step5best[1:SIZE]<-step5repeatpass$retparam1tries[1]*exp(-((meanages[]-step5repeatpass$retparam3tries[1])/step5repeatpass$retparam2tries[1])*((meanages[]-step5repeatpass$retparam3tries[1])/step5repeatpass$retparam2tries[1]))
 step5<-step4+step5best
 
-##STEP 6 FIT - SELECT BEST PERCENT PARAMETER VALUES OF TRIES BASED ON INPUT DISTRIBUTIONS, THEN REPEAT TRIES WITH THE UNIFORM BOUNDS OF BEST PERCENT UNTIL CONVERGENCE  
-step6triesfit<-function(eldparam1tries,eldparam2tries){
+##STEP 6 FIT - SELECT BEST PERCENT PARAMETER VALUES OF TRIES BASED ON INPUT DISTRIBUTIONS, THEN REPEAT TRIES WITH THE UNIFORM BOUNDS OF BEST PERCENT UNTIL CONVERGENCE   
+step6triesfit<-function(eldparam1tries,eldparam2tries,eldparam3tries){
 step6tries<-array(step1-step2,dim=c(length(step1),TRIES))
-for (i in 1:TRIES) {step6tries[1:SIZE,i]<-eldparam1tries[i]*exp(eldparam2tries[i]*meanages[])}
+for (i in 1:TRIES) {step6tries[1:SIZE,i]<-eldparam1tries[i]*exp(-((meanages[]-eldparam3tries[i])/eldparam2tries[i])*((meanages[]-eldparam3tries[i])/eldparam2tries[i]))}
 eldresidtries<-array(0,dim=c(length(step1),TRIES))
-for (i in 1:TRIES) {for (j in 1:length(meanages)) {if((meanages[j]>=eldmin)&(meanages[j]<=eldmax)) {eldresidtries[j,i]<-(step6tries[j,i]-(step1-step5)[j])^2}}}
+for (i in 1:TRIES) {for (j in 1:length(meanages)) {if((meanages[j]>=eldmin)&(meanages[j]<=eldmax)&((meanages[j]<min(studentages))|(meanages[j]>max(studentages)))) {eldresidtries[j,i]<-(step6tries[j,i]-(step1-step4)[j])^2}}}
 sumeldresidtries<-array(,TRIES)
 for (i in 1:TRIES) {sumeldresidtries[i]<-sum(eldresidtries[,i])}
 eldparam1tries<-runif(TRIES,min(eldparam1tries[match(head(sort(sumeldresidtries),TRIES*BEST),sumeldresidtries)]),max(eldparam1tries[match(head(sort(sumeldresidtries),TRIES*BEST),sumeldresidtries)]))
 eldparam2tries<-runif(TRIES,min(eldparam2tries[match(head(sort(sumeldresidtries),TRIES*BEST),sumeldresidtries)]),max(eldparam2tries[match(head(sort(sumeldresidtries),TRIES*BEST),sumeldresidtries)]))
-eldparamtries<-data.frame(sumeldresidtries=sumeldresidtries,eldparam1tries=eldparam1tries,eldparam2tries=eldparam2tries)
+eldparam3tries<-runif(TRIES,min(eldparam3tries[match(head(sort(sumeldresidtries),TRIES*BEST),sumeldresidtries)]),max(eldparam3tries[match(head(sort(sumeldresidtries),TRIES*BEST),sumeldresidtries)]))
+eldparamtries<-data.frame(sumeldresidtries=sumeldresidtries,eldparam1tries=eldparam1tries,eldparam2tries=eldparam2tries,eldparam3tries=eldparam3tries)
 return(c(step6tries,eldparamtries))
 }
-step6repeatpass<-step6triesfit(eldparam1tries,eldparam2tries)
+step6repeatpass<-step6triesfit(eldparam1tries,eldparam2tries,eldparam3tries)
 ITER<-0
-while (abs(max(step6repeatpass$eldparam1tries)-min(step6repeatpass$eldparam1tries))>FITTO &
-abs(max(step6repeatpass$eldparam2tries)-min(step6repeatpass$eldparam2tries))>FITTO
+while (abs(max(step6repeatpass$eldparam1tries)-min(step6repeatpass$eldparam1tries))>FITTO & 
+abs(max(step6repeatpass$eldparam2tries)-min(step6repeatpass$eldparam2tries))>FITTO & 
+abs(max(step6repeatpass$eldparam3tries)-min(step6repeatpass$eldparam3tries))>FITTO  
 )
-{step6repeatpass<-step6triesfit(step6repeatpass$eldparam1tries,step6repeatpass$eldparam2tries)
+{step6repeatpass<-step6triesfit(step6repeatpass$eldparam1tries,step6repeatpass$eldparam2tries,step6repeatpass$eldparam3tries)
 ITER=ITER+1
 }
 step6repeatpass$eldparam1tries[1]
 step6repeatpass$eldparam2tries[1]
+step6repeatpass$eldparam3tries[1]
 step6repeatpass$sumeldresidtries[1]
 ITER
-step6best<-array(step1-step5,dim=c(length(step1)))
-step6best[1:SIZE]<-step6repeatpass$eldparam1tries[1]*exp(step6repeatpass$eldparam2tries[1]*meanages[])
+step6best<-array(step1-step4,dim=c(length(step1)))
+step6best[1:SIZE]<-step6repeatpass$eldparam1tries[1]*exp(-((meanages[]-step6repeatpass$eldparam3tries[1])/step6repeatpass$eldparam2tries[1])*((meanages[]-step6repeatpass$eldparam3tries[1])/step6repeatpass$eldparam2tries[1]))
 step6<-step5+step6best
 
 ##STEP 7 FIT - SELECT BEST PERCENT PARAMETER VALUES OF TRIES BASED ON INPUT DISTRIBUTIONS, THEN REPEAT TRIES WITH THE UNIFORM BOUNDS OF BEST PERCENT UNTIL CONVERGENCE  
